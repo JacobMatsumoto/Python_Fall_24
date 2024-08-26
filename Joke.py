dabdd9aef72cfff74b6fc2fe80cb62771e7e6c56
JacobMatsumoto <jmatsumoto@students.mchenry.edu>
2024-08-26 14:11:28 -0500

    Add files via upload

---
+++ Joke.py
@@ -0,0 +1,10 @@
+# Assignment #1 Knock Knock?
+print("Knock, Knock")
+print("Who's there?")
+print("Cows go")
+print("Cows go who?")
+print("Owl's go who, Cows go moo!")
+print("Knock, Knock", "\nWho's there?", "\nWooden shoe", "\nWooden shoe who?", "\nWooden shoe like to hear another joke?")
+#using the escape character \n you can put multiples lines in one block of code
+print("Knock, Knock", "\tWho's there?", "\tAtch", "\tAtch who?", "\tBless  You!")
+#\t puts large tab spaces
