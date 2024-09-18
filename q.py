3ecdc6593825fc416770eae1a3d4d5bb1d67c764
Jacob <jmatsumoto@students.mchenry.edu>
2024-09-18 14:01:58 -0500

    started q3.py fixed spacing on q.py and did the lesson on q.2

--- q.py
+++ q.py
@@ -7,7 +7,7 @@ while count > 1:
     print(f"{count} bottles of beer on the wall " f"\n{count} bottles of beer" "\nTake one down pass it around")
     count -= 1 #incrementing it to count down
     if count == 1: #insurance for grammar on 1 bottle
-         print(f"{count} bottle of beer on the wall")
-    else:print(f"{count} bottles of beer on the wall")
+         print(f"{count} bottle of beer on the wall" "\n")
+    else:print(f"{count} bottles of beer on the wall" "\n")
     if count == 1:
          print(f"{count} bottle of beer on the wall"f"\n{count} bottle of beer""\nTake it down pass it around" "\nNo more bottles of beer on the wall!")
