e85af992d54134bdab96c4c5dea4df066fce1157
Jacob <jmatsumoto@students.mchenry.edu>
2024-09-18 14:09:23 -0500

    renamed q3.p to divide_by_7  and did the assignment

---
+++ divide_by_7.py
@@ -0,0 +1,7 @@
+"""Use a for loop to iterate through the range of numbers from 1 to 300.
+Inside the loop, use an if statement to check if a number is divisible by 7. To do this, use the modulus operator (%) and compare the remainder with 0.
+If a number is divisible by 7, print that number.
+Ensure your program outputs each qualifying number on a separate line."""
+for i in range (1, 300):
+    if i % 7 == 0:
+        print(i)
