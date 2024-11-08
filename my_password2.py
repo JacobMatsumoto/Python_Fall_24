04b9dafa8e4afb1daba5bf3cbb21923f0c28fb1d
Jacob <jmatsumoto@students.mchenry.edu>
2024-11-08 15:35:20 -0600

    fixed

--- my_password2.py
+++ my_password2.py
@@ -12,36 +12,36 @@ def main():
             valid_length = True
         else:
             print("Please enter a password with a length of 8-20")
-            main()
+            continue
         for character in user_pass:
             if character.isupper() == True:
                 valid_cap = True  # checks upper, functioning
         if valid_cap == False:
             print("Please use an upper case letter")
-            main()
+            continue
 
         for character in user_pass:
             if character.islower() == True:
                 valid_lower = True
         if valid_lower == False:  # checks lower, functioning
             print("please use a lower case letter")
-            main()
+            continue
 
         for character in user_pass:
             if character in ('1', '2', '3', '4', '5', '6', '7', '8', '9', '0'):
                 valid_num = True
         if valid_num == False:  # checks num, functioning
             print("Please use a number")
-            main()
+            continue
 
         for character in user_pass:
             if character in ('!', '@''#', '$', '%', '&', '*'):
                 valid_special = True
         if valid_special == False:  # checks special character, functioning
             print("Please use a special character ex.(!@#$%&*)")
-            main()
+            continue
 
-        if valid_length and valid_cap and valid_lower and valid_special and valid_num == True:
+        if (valid_length and valid_cap and valid_lower and valid_special and valid_num) == True:
             confirmed_password = input(
                 "Please re-enter your password to confirm ")
             if confirmed_password == user_pass:  # for some reason the above code moves any time I run or save
@@ -49,7 +49,7 @@ def main():
                 valid = False  # This wont break the loop for some reason, and I dont feel a break is appropriate
             else:
                 print("Sorry re-enter your password")  # reseting works
-                main()
+                continue
 
 
 main()
