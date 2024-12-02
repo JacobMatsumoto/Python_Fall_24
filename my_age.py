477d06db0ddfac7230b1a2aec9aae8ece3dcb657
Jacob <jmatsumoto@students.mchenry.edu>
2024-12-02 14:08:19 -0600

    age assignment done

---
+++ my_age.py
@@ -0,0 +1,41 @@
+# calculate a person's age in: Years, months, weeks, days
+# 30.41 days in a month
+# 7 days in a week
+
+from datetime import datetime
+
+
+def main():
+
+    print("\n\n")
+    try:
+        today = datetime.today()
+        birth_year = int(input("What year were you born?  (4 digits - 1972) "))
+        month = int(
+            input("What Month were you born (as a number. May would be 5)  "))
+        day = int(input("What day of the month were you born?  "))
+        # just need datetime not datetime.date
+        # because we imported datetime from datetime
+        birthday = datetime(birth_year, month, day)
+        print("Your birthday is: ")
+        birthday_output = birthday.strftime("%Y-%m-%d")
+        print(birthday_output)
+
+        delta = today - birthday
+        print(f'Difference is {delta.days} days')
+        delta_years = delta.days // 365.2425
+
+        print(f'You are {delta_years} years old')
+
+        delta_months = delta.days // 30.41
+        delta_weeks = delta.days // 7
+
+        print(f"You are {delta_months} months old")
+        print(f"You are {delta_weeks} weeks old")
+
+    except Exception as e:
+        print(f"ooooops!!!:  {e}")
+        main()
+
+
+main()
--- practice9/12_2practice.py
+++ practice9/12_2practice.py
@@ -1,10 +1,24 @@
-import os
+# import os
+
+
+# def main():
+#     # os.mkdir("ducks")
+#     # os.mkdir("ducks/real")
+#     # os.mkdir("ducks/cartoon")
+#     # os.mkdir("ducks/not_ducks")
+
+#     # main()
+from datetime import datetime
 
 
 def main():
-    # os.mkdir("ducks")
-    # os.mkdir("ducks/real")
-    # os.mkdir("ducks/cartoon")
-    # os.mkdir("ducks/not_ducks")
 
-    # main()
+    # d = datetime.date(2023, 4, 13)
+    # print("date and time", d)
+    # now = datetime.now()
+    # print("now", now)
+    timestamp = datetime.now().time()
+    print(timestamp)
+
+
+main()
