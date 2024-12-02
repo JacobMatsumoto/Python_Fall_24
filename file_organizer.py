d049e2eb926ffb1fff671032a2a401658170f7a6
Jacob <jmatsumoto@students.mchenry.edu>
2024-12-02 13:24:56 -0600

    file file_organizer done

---
+++ file_organizer.py
@@ -0,0 +1,11 @@
+import os
+
+
+def main():
+    # os.mkdir('MyFiles')
+    # os.mkdir('MyFiles/Docs')
+    # os.mkdir('MyFiles/Photos')
+    # os.mkdir('MyFiles/Videos')
+
+
+main()
---
+++ practice9/12_2practice.py
@@ -0,0 +1,10 @@
+import os
+
+
+def main():
+    # os.mkdir("ducks")
+    # os.mkdir("ducks/real")
+    # os.mkdir("ducks/cartoon")
+    # os.mkdir("ducks/not_ducks")
+
+    # main()
